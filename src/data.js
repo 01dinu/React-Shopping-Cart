8e2ab80cb92b8703d5600470b75b7aba966b3634
Dinusha Edirisinghe <90330425+01dinu@users.noreply.github.com>
2023-03-29 00:41:52 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,23 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Macbook',
+      price: 450000,
+      image: 'https://picsum.photos/id/180/2400/1600',
+    },
+    {
+      id: '2',
+      name: 'Cup',
+      price: 2000,
+      image: 'https://picsum.photos/id/30/2400/1600',
+    },
+    {
+      id: '1',
+      name: 'Glasses',
+      price: 25000,
+      image: 'https://picsum.photos/id/201/2400/1600',
+    },
+  ],
+};
+export default data;
